9dc235c9953fa4f1899beed766ddb2917076929d
cristian <cefunck@miuandes.cl>
2019-05-06 00:02:35 -0400

    added tests for cases where the dimensions of the block model are not divisible by the reblock factors

--- BlockModelTests.py
+++ BlockModelTests.py
@@ -478,5 +478,117 @@ class BlockModelReblockWithInvalidArguments(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.blockModel.reblock_model(-1, 1, 1)
 
+class BlockModelReblockWithDimensionsThatAreNotDivisibleByReblockFactors(unittest.TestCase):
+    def setUp(self):
+        self.block1 = Block("0,0,0"
+                            , 0
+                            , 0
+                            , 0
+                            , float(1000)
+                            , {"Au": {"value": float(100), "grade_type": 1}
+                                , "Cu": {"value": float(100), "grade_type": 2}
+                                , "Ag": {"value": float(100), "grade_type": 3}
+                                , "Li": {"value": float(100), "grade_type": 4}}
+                            )
+        self.block2 = Block("8,2,4"
+                            , 8
+                            , 2
+                            , 4
+                            , float(1000)
+                            , {"Au": {"value": float(100), "grade_type": 1}
+                                , "Cu": {"value": float(100), "grade_type": 2}
+                                , "Ag": {"value": float(100), "grade_type": 3}
+                                , "Li": {"value": float(100), "grade_type": 4}
+                               }
+                            )
+
+        self.block3 = Block("6,4,2"
+                            , 6
+                            , 4
+                            , 2
+                            , float(1000)
+                            , {"Au": {"value": float(100), "grade_type": 1}
+                                , "Cu": {"value": float(100), "grade_type": 2}
+                                , "Ag": {"value": float(100), "grade_type": 3}
+                                , "Li": {"value": float(100), "grade_type": 4}
+                               }
+                            )
+
+        self.blocks = [self.block1,
+                       self.block2,
+                       self.block3]
+
+        self.mineralDeposit = MineralDeposit("testMineralDeposit",
+                                                           0,
+                                                           1,
+                                                           2,
+                                                           3,
+                                                           {"Au":{"mineral_column": 4,"grade_type": 1}}
+                                                           )
+
+        self.blockModel = BlockModel("testBlockModel", self.blocks, self.mineralDeposit.name)
+
+        self.newCoordinates = (0,0,0)
+
+        x, y, z = self.newCoordinates[0], self.newCoordinates[1], self.newCoordinates[2]
+        newId = str(x) + "," + str(y) + "," + str(z)
+        newWeight = 3000
+        newGrades = {"Au": {"value": float(300), "grade_type": 1}
+                        , "Cu": {"value": float(100), "grade_type": 2}
+                        , "Ag": {"value": float(100), "grade_type": 3}
+                        , "Li": {"value": float(100), "grade_type": 4}
+                     }
+        self.correctCombinedBlock = Block(newId
+                            , x
+                            , y
+                            , z
+                            , newWeight
+                            , newGrades
+                            )
+
+    def test_block_model_reblock_model_by_zero_at_x_coordinate(self):
+        with self.assertRaises(ValueError):
+            self.blockModel.reblock_model(0, 1, 1)
+
+    def test_block_model_reblock_model_by_zero_at_y_coordinate(self):
+        with self.assertRaises(ValueError):
+            self.blockModel.reblock_model(1, 0, 1)
+
+    def test_block_model_reblock_model_by_zero_at_z_coordinate(self):
+        with self.assertRaises(ValueError):
+            self.blockModel.reblock_model(1, 1, 0)
+
+    def test_block_model_reblock_model_by_zero_at_all_coordinates(self):
+        with self.assertRaises(ValueError):
+            self.blockModel.reblock_model(0, 0, 0)
+
+    def test_block_model_reblock_model_not_divisible_at_x_coordinate(self):
+        old_block_quantity = self.blockModel.count_blocks()
+        self.blockModel.reblock_model(3, 2, 2)
+        new_block_quantity = self.blockModel.count_blocks()
+        self.assertTrue(old_block_quantity >= new_block_quantity,
+                        "is not being filled correctly with air blocks when it is not divisible at x coordinate")
+
+    def test_block_model_reblock_model_not_divisible_at_y_coordinate(self):
+        old_block_quantity = self.blockModel.count_blocks()
+        self.blockModel.reblock_model(2, 3, 2)
+        new_block_quantity = self.blockModel.count_blocks()
+        self.assertTrue(old_block_quantity >= new_block_quantity,
+                        "is not being filled correctly with air blocks when it is not divisible at y coordinate")
+
+    def test_block_model_reblock_model_not_divisible_at_z_coordinate(self):
+        old_block_quantity = self.blockModel.count_blocks()
+        self.blockModel.reblock_model(2, 2, 3)
+        new_block_quantity = self.blockModel.count_blocks()
+        self.assertTrue(old_block_quantity >= new_block_quantity,
+                        "is not being filled correctly with air blocks when it is not divisible at z coordinate")
+
+    def test_block_model_reblock_model_not_divisible_at_all_coordinates(self):
+        old_block_quantity = self.blockModel.count_blocks()
+        self.blockModel.reblock_model(3, 3, 3)
+        new_block_quantity = self.blockModel.count_blocks()
+        self.assertTrue(old_block_quantity >= new_block_quantity,
+                        "is not being filled correctly with air blocks when it is not divisible at all coordinates")
+
 if __name__ == '__main__':
     unittest.main()
